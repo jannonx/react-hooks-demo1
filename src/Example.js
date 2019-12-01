4bda42e6a7c191f6f3b0a2f6222501b966d83a78
jannonx <791273579@qq.com>
2019-12-01 17:05:43 +0800

    useEffect 第二个参数

--- src/Example.js
+++ src/Example.js
@@ -1,9 +1,33 @@
-import React, { useState ,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+
+function Index() {
+    useEffect(()=>{
+        console.log(`Index...useEffect=>老弟，你来了！Index页面`)
+
+        return ()=>{//解绑
+            console.log(`Index...useEffect=>老弟，你走了！Index页面`)
+        }  
+    },[])//空，被销毁解绑才执行;非空，比如count状态发生改变，都会执行
+    return (
+        <h2>JAnonx blog</h2>
+    )
+}
+
+function List() {
+    useEffect(()=>{
+        console.log(`List...useEffect=>老弟，你来了！List页面`)
+    })
+    return (
+        <h2>List Page</h2>
+    )
+}
 
 function Example() {
     const [count, setCount] = useState(0)//数组解构
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log(`useEffect=>You clicked ${count} times`)
     })
 
@@ -16,6 +40,16 @@ function Example() {
             }>
                 Click Me
           </button>
+
+            <Router >
+                <ul>
+                    <li><Link to="/">首页</Link></li>
+                    <li><Link to="/list">列表</Link></li>
+                </ul>
+
+                <Route path="/" exact component={Index} />
+                <Route path="/list/" component={List} />
+            </Router>
         </div>
     )
 }
