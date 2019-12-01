38f6ac9b54fefd64b46256e88bbb7e43565199bf
jannonx <791273579@qq.com>
2019-12-01 16:26:19 +0800

    useState多状态,不能存在条件语句中

--- src/Example.js
+++ src/Example.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function Example() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0)//数组解构
 
     return (
         <div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './Example';
+import Example2 from './Example2';
 
 
-ReactDOM.render(<Example />, document.getElementById('root'));
+ReactDOM.render(<Example2 />, document.getElementById('root'));
 
 
